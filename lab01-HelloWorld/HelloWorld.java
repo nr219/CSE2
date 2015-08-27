62129d1005a337fc42636e1a1f1506205ee5d38d
ntr219 <ntr219@lehigh.edu>
2015-08-27 18:00:00 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//
+// Nicholai Redfern
+// Lab001
+// HelloWorld.java
+
+// class
+public class HelloWorld{
+    
+//main
+    public static void main(String[] args){
+        
+        System.out.println("###################");
+        System.out.println("###################");
+        System.out.println("##               ##");
+        System.out.println("##  Hello World  ##");
+        System.out.println("##               ##");
+        System.out.println("###################");
+        System.out.println("###################");
+        System.out.println("testing");
+    
+    
+//test
+    }
+}
