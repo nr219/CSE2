85875ad356eaae603e2bf63fbd9a8a90ed7455b7
ntr219 <ntr219@lehigh.edu>
2015-09-02 01:07:56 +0000

    updated HelloClass

---
+++ Class/Speed.java
@@ -0,0 +1,16 @@
+public class
+
+public static void main(String[] args){
+    
+    float processorSpeed;
+    byte numberProccesors;
+    float computerSpeed;
+    
+     processorSpeed= 2.3f;
+     numberProccesors= 3;
+    
+    computerSpeed = processorSpeed + numberProccesors;
+    
+    
+}
+
